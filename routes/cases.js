8be949bd48d52cb36710caeddf1570090e77f07c
camocros <sernaarciniegas@gmail.com>
2026-06-09 20:26:27 -0600

    Temporarily remove isAuthenticated for W03 video recording

--- routes/cases.js
+++ routes/cases.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const casesController = require('../controllers/cases');
-const isAuthenticated = require('../middleware/isAuthenticated');
 
 router.get('/', casesController.getAll);
 router.get('/:id', casesController.getSingle);
-router.post('/', isAuthenticated, casesController.createCase);
-router.put('/:id', isAuthenticated, casesController.updateCase);
-router.delete('/:id', isAuthenticated, casesController.deleteCase);
+router.post('/', casesController.createCase);
+router.put('/:id', casesController.updateCase);
+router.delete('/:id', casesController.deleteCase);
 
 module.exports = router;
--- routes/materials.js
+++ routes/materials.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const materialsController = require('../controllers/materials');
-const isAuthenticated = require('../middleware/isAuthenticated');
 
 router.get('/', materialsController.getAll);
 router.get('/:id', materialsController.getSingle);
-router.post('/', isAuthenticated, materialsController.createMaterial);
-router.put('/:id', isAuthenticated, materialsController.updateMaterial);
-router.delete('/:id', isAuthenticated, materialsController.deleteMaterial);
+router.post('/', materialsController.createMaterial);
+router.put('/:id', materialsController.updateMaterial);
+router.delete('/:id', materialsController.deleteMaterial);
 
 module.exports = router;
